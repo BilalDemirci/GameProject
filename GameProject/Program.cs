c9b51890a0aa2126a6029d33e1438b4ab845005b
Bilal Demirci <dmrc45500@icloud.com>
2024-04-14 14:32:46 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using GameProject;
+
+GamerManager gamerManager = new GamerManager(new UserValidationManager());
+
+Gamer gamer1 = new Gamer();
+gamer1.Id = 1;
+gamer1.FirstName = "Bilal";
+gamer1.LastName = "Demirci";
+gamer1.DateOfBirth = 1999;
+gamer1.IdentityNumber = "61075";
+
+Game game1 = new Game();
+game1.GameId = 1;
+game1.GameName = "Counter Strike2";
+game1.GamePrice = 15;
+
+gamerManager.Add(gamer1);
+GameSalesManager gameSalesManager = new GameSalesManager();
+gameSalesManager.Sale(gamer1,game1);
+
+Console.ReadLine();
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.DateOfBirth == 1999 && gamer.FirstName == "Bilal" && gamer.LastName == "Demirci" && gamer.IdentityNumber =="61075")
+            {
+                return true;
+            }
+            else {  return false; }
+        }
+    }
+}
